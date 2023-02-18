4c7d365ee49fb740a49e98d60d440cb0c23a44a7
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-02-18 18:05:14 +0100

    change navigate in onbording page

--- mobile/pages/Onboarding.jsx
+++ mobile/pages/Onboarding.jsx
@@ -22,8 +22,8 @@ const Done = ({ ...props }) => (
 const OnboardingScreen = ({ navigation }) => {
   return (
     <Onboarding
-      onSkip={() => navigation.replace("Nav")}
-      onDone={() => navigation.navigate("Nav")}
+      onSkip={() => navigation.replace("Login")}
+      onDone={() => navigation.navigate("Login")}
       SkipButtonComponent={Skip}
       NextButtonComponent={Next}
       DoneButtonComponent={Done}
