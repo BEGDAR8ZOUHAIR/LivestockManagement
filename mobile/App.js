a6c54e38fb8f864c459dde261657d3b9d6c6351f
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-02-18 18:02:42 +0100

    add  logBox

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, } from 'react-native';
+import { StyleSheet, Text, View, LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaView } from 'react-native';
@@ -11,7 +11,7 @@ const Stack = createStackNavigator();
 
 export default function App()
 {
-
+  LogBox.ignoreLogs(['Remote debugger']);
 
   return (
 
